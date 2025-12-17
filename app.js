f162fbaee071e49ebddffdfabbfc014cde0ba4c1
EfraimGottlieb <efgo583208979@gmail.com>
2025-12-17 10:21:17 +0200

    added func getPeopleByName(name)

--- app.js
+++ app.js
@@ -4,15 +4,34 @@ import file from "./utils/fileHandling.js";
 
 const baseUrl = "https://spies-test-server.vercel.app/";
 
-async function downloadAndSavePeople() {
-  const people = await api.get(baseUrl + 'people');
-  file.write("data/people.json", JSON.stringify(people));
+async function fetchPeople() {
+  const people = await api.get(baseUrl + "people");
+  file.write("data/PEOPLE.json", JSON.stringify(people));
 }
 
-async function downloadAndSaveRecords() {
-  const records = await api.get(baseUrl + 'transcriptions')
-  file.write("data/TRANSCRIPTIONS.json", JSON.stringify(records))
+async function fetchRecords() {
+  const records = await api.get(baseUrl + "transcriptions");
+  file.write("data/TRANSCRIPTIONS.json", JSON.stringify(records));
 }
 
-downloadAndSavePeople()
-downloadAndSaveRecords()
+async function getPeople() {
+  let people = await file.read("data/PEOPLE.json");
+  people = JSON.parse(people);
+  return people;
+}
+
+async function getPeopleByName(name) {
+  const people = await getPeople();
+  const result = people.filter(
+    (people) => people.name.toLowerCase() == name.toLowerCase()
+  );
+  if (result.length === 0) {
+    console.log(`${name} not found!`);
+    return;
+  }
+  console.log(result[0])
+}
+fetchPeople();
+fetchRecords();
+const name = "leah";
+getPeopleByName(name)
